4326942c97648c2501aa9b634943c46c9af04fd7
Alberto M. Ticianelli <67422877+amticianelli@users.noreply.github.com>
2025-03-05 18:25:46 +0000

    Adding vertexAI

--- backend/routers/openaiInteraction.py
+++ backend/routers/openaiInteraction.py
@@ -11,6 +11,12 @@ import io
 from openai.types.chat.chat_completion import ChatCompletion
 import yaml
 
+# VertexAI integration
+import vertexai
+from vertexai.generative_models import GenerativeModel, Part, FinishReason
+import vertexai.preview.generative_models as generative_models
+
+
 router = APIRouter(
     prefix="/openai",
     tags=["openai"]
@@ -20,13 +26,26 @@ router = APIRouter(
 security = HTTPBasic()
 
 # Setting auth credentials
+# ENGINE
+ENGINE = os.getenv("ENGINE",'OPENAI')
+
+# AZ Open AI
 API_BASE = os.getenv("AZURE_OPENAI_ENDPOINT")
 API_KEY= os.getenv("AZURE_OPENAI_API_KEY")
 DEPLOYMENT_NAME= os.getenv("MODEL_NAME")
+API_VERSION = os.getenv('API_VERSION','2023-12-01-preview')
 
+# Vertex
+VERTEX_MODEL = os.getenv('VERTEX_MODEL','gemini-2.0-flash-exp')
+VERTEX_KEYPATH = os.getenv('KEYPATH','./gcloud_key.json')
+VERTEX_PROJECT = os.getenv('VERTEX_PROJECT','adeconnect-dev')
+VERTEX_LOC = os.getenv('VERTEX_LOC','us-central1')
+
+#BR Gov
 BRGOV_API_KEY = os.getenv("BRGOV_API_KEY")
 BRGOV_ENDPOINT = os.getenv("BRGOV_ENDPOINT")
 
+# FastAPI
 PREDEFINED_SECRET = os.getenv("PREDEFINED_SECRET")
 
 # Obtaining prompts
@@ -138,7 +157,7 @@ def azopaiRequest(fileInfo: dict):
     encoded_image = base64.b64encode(imgByteArr.getvalue()).decode('ascii')
     
     deployment_name = DEPLOYMENT_NAME
-    api_version = '2023-12-01-preview' # this might change in the future
+    api_version = API_VERSION # this might change in the future
 
     client = AzureOpenAI(
         api_key=API_KEY,  
@@ -187,9 +206,82 @@ def azopaiRequest(fileInfo: dict):
     return invoiceData
 
 
+def vertexRequest(fileInfo: dict) -> dict:
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = VERTEX_KEYPATH
+
+    fileName: str = fileInfo["filename"]
 
 
-@router.post("/convertInvoice")
+    image: Image.Image = fileInfo["image"][0] # Getting the first image only
+
+    print(f'Image size: {image.size} Image type: {type(image)}')
+
+
+    imgByteArr = io.BytesIO()
+    image.save(imgByteArr, format='PNG')
+
+    # DEBUG
+    image.save(f'./{fileName}'.replace('pdf','jpeg')) # DEBUG
+
+    print(f'File to be processed: {fileName}, size: {imgByteArr.getbuffer().nbytes}')
+       
+
+    encoded_image = base64.b64encode(imgByteArr.getvalue()).decode('ascii')
+
+    image1 = Part.from_data(
+        mime_type="image/jpeg",
+        data=base64.b64decode(encoded_image))
+
+    generation_config = {
+        "max_output_tokens": 2048,
+        "top_p": 0.4,
+        "top_k": 32,
+        "temperature": 0
+        }
+
+    safety_settings = {
+        generative_models.HarmCategory.HARM_CATEGORY_HATE_SPEECH: generative_models.HarmBlockThreshold.BLOCK_ONLY_HIGH,
+        generative_models.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: generative_models.HarmBlockThreshold.BLOCK_ONLY_HIGH,
+        generative_models.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: generative_models.HarmBlockThreshold.BLOCK_ONLY_HIGH,
+        generative_models.HarmCategory.HARM_CATEGORY_HARASSMENT: generative_models.HarmBlockThreshold.BLOCK_ONLY_HIGH,
+    }
+
+    vertexai.init(project=VERTEX_PROJECT, location=VERTEX_LOC)
+    
+    model = GenerativeModel(VERTEX_MODEL)
+
+    response = model.generate_content(
+        [PROMPT_CONF['systemMessage']+PROMPT_CONF['mainPrompt'] + PROMPT_CONF['result_output'], image1],
+        generation_config=generation_config,
+        safety_settings=safety_settings,
+        stream=False,
+
+    )
+
+    #print(response.text)
+
+    jsonResult = response.text \
+                .replace("```","") \
+                .replace("json","") \
+                .replace("\n","")
+  
+    invoiceData: dict = json.loads(jsonResult)
+
+    if invoiceData is not None:
+        if 'prestador' in invoiceData:
+            checkCompany: dict = getCompanyStatus(invoiceData['prestador']['cpf_cnpj'])
+
+            if 'Error' not in checkCompany:
+                print(checkCompany)
+                invoiceData['situacao'], invoiceData['tipo_empresa'] = checkCompany['situacao'],checkCompany['tipoempresa']
+            else:
+                 print(checkCompany)
+    return invoiceData
+
+
+
+
+@router.post("/convertInvoice/")
 def convertInvoice(files: list[UploadFile], user: str = Depends(verify_credentials)):
     
     print(f'User: {user}')
@@ -200,7 +292,7 @@ def convertInvoice(files: list[UploadFile], user: str = Depends(verify_credentia
     responseList: list[dict] = []
 
     for file in listFiles:
-        responseList.append(azopaiRequest(file)) 
+        responseList.append(azopaiRequest(file)) if ENGINE == 'OPENAI' else responseList.append(vertexRequest(file)) 
 
     
 
